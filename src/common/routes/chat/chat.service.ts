90d05ee721def2c465c723d12cf67be694a13428
sangwoolee <a26484638@gmail.com>
2025-08-06 21:05:54 +0900

    commit

--- src/common/routes/chat/chat.service.ts
+++ src/common/routes/chat/chat.service.ts
@@ -224,7 +224,7 @@ export class ChatService {
         'sender.nickname',
         'profile.profile_image_url',
       ])
-      .orderBy('message.created_at', 'DESC')
+      .orderBy('message.created_at', 'ASC')
       .take(limit)
       .skip((page - 1) * limit);
 
@@ -511,7 +511,7 @@ export class ChatService {
         'sender.nickname',
         'profile.profile_image_url',
       ])
-      .orderBy('message.created_at', 'DESC')
+      .orderBy('message.created_at', 'ASC')
       .take(limit)
       .skip((page - 1) * limit);
 
