f09193beeac2be79d6f4bfd85a3b34dd1b62e3ad
sangwoolee <a26484638@gmail.com>
2025-08-07 19:40:51 +0900

    commit

--- src/common/routes/challengeroom/challengeroom.module.ts
+++ src/common/routes/challengeroom/challengeroom.module.ts
@@ -5,11 +5,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ChallengeRoom } from 'src/common/entities/challenge_room.entity';
 import { ChallengeParticipant } from 'src/common/entities/challenge_participant.entity';
 import { User } from 'src/common/entities/user.entity';
+import { UserProfile } from 'src/common/entities/user_profile.entity';
 import { ChallengeScheduler } from './challengeroom.scheduler';
+import { Ranking } from 'src/common/entities/ranking.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([ChallengeRoom, ChallengeParticipant, User]),
+    TypeOrmModule.forFeature([ChallengeRoom, ChallengeParticipant, User, UserProfile, Ranking]),
   ],
   controllers: [ChallengeroomController],
   providers: [ChallengeroomService, ChallengeScheduler],
